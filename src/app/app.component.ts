a3e31c0885206b37a0336512b3be400e84431fc8
Ragul <ragul0712@gmail.com>
2022-08-25 14:55:32 +0530

    v1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-task-list';
+export class AppComponent implements OnInit {
+  title = 'My Task List';
+  taskList: string []= [] ;
+
+  ngOnInit():void {
+    if(localStorage.getItem('my_tasks')){
+      this.taskList = JSON.parse(localStorage.getItem('my_tasks'))
+    }
+  }
+  
 }
---
+++ src/app/task-form/task-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-form',
+  templateUrl: './task-form.component.html',
+  styleUrls: ['./task-form.component.css']
+})
+export class TaskFormComponent implements OnInit {
+  @Input() taskList;
+  newTask: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addTODO(){
+    this.taskList.unshift(this.newTask);
+    localStorage.setItem('my_tasks',JSON.stringify(this.taskList))
+    this.newTask= '';
+  }
+
+}
